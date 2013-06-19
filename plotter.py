66bf80c48d8d594e9023e651e33eb40ba5261a2b
bd-j <benjamin.duncan.johnson@gmail.com>
2013-06-19 11:14:01 +0200

    text on binning

--- plotter.py
+++ plotter.py
@@ -1,15 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as pl
 
-def plot_sfh(pars, outsamples, angst_sfh):
+def plot_sfh(pars, outsamples, angst_sfh, fine_sfh = None):
     # SFH
     delta = ( (angst_sfh - outsamples).std(axis = 0) / angst_sfh ).mean()
     ns = pars['nsample']
-    pl.plot(np.log10(pars['rebin_centers']*1e9),angst_sfh,
+
+    if fine_sfh is not None:
+        xx = np.log10( np.append(pars['bin_ends'], pars['bin_starts'][-1]) )
+        xx[0] = 6.0
+        pl.step(xx, np.append(fine_sfh,0), where = 'post',
+            linewidth = 2, color = 'green', label = 'fine_input')
+
+            #        pl.plot(np.log10(pars['bin_centers']*1e9),fine_sfh,
+            #linewidth = 2, color = 'green', label = 'fine input', alpha = 0.7)
+
+#    pl.plot(np.log10(pars['rebin_centers']*1e9),angst_sfh,
+#            linewidth = 2, color = 'red', label = 'input')
+    xx = np.log10( np.append(pars['rebin_ends'], pars['rebin_starts'][-1]) )
+    xx[0] = 6.0
+    pl.step(xx, np.append(angst_sfh,0), where = 'post',
             linewidth = 2, color = 'red', label = 'input')
+
     for i in np.random.uniform(size=30) :
         pl.plot(np.log10(pars['rebin_centers']*1e9),outsamples[np.floor(i * ns),:],
                 color = 'grey', alpha = 0.5, label = 'post. sample')
+
 
     #pl.legend()
     pl.xlabel('lookback time')
@@ -74,7 +90,7 @@ def plot_sfr_mass(pars, outsamples, angst_sfh, sfr_time_index = 3):
     pl.savefig("{figname}_sfr.png".format(**pars))
     pl.close()
     
-def plot_covariance(pars, outsamples, bin1 = 0, bin2 = 1):
+def plot_covariance(pars, outsamples, bin1 = 0, bin2 = 1, sfh_input = None):
     # COMPONENT COVARIANCE
     
     pl.plot(outsamples[:,bin1], outsamples[:,bin2],'ro', alpha = 0.1, mec ='red')
@@ -82,8 +98,13 @@ def plot_covariance(pars, outsamples, bin1 = 0, bin2 = 1):
     pl.xlabel(r'SFR$_{{{0:.1f}}}$'.format(np.log10(pars['rebin_centers'][bin1]*1e9)) )
     pl.ylabel(r'SFR$_{{{0:.1f}}}$'.format(np.log10(pars['rebin_centers'][bin2]*1e9)) )
     pl.title(r"{name}: $\lambda\lambda = {wlo},{whi}$, SNR = {snr}, $\sigma_v ={veldisp}$ km/s".format(**pars))
+    print(sfh_input is not None)
+
+    if sfh_input is not None:
+        pl.plot(sfh_input[bin1],sfh_input[bin2],'go', alpha = 1.0, mec = 'green')
 
     pl.savefig("{figname}_covar.png".format(**pars))
     pl.close()
+
 # CHAIN FOR EARLIEST SFR COMPONENT
 #for i in np.random.uniform(size=ndim) : pl.plot(sampler.chain[np.floor(i*nwalkers),:,-1] )
