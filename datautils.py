3a0133be7ad92690385bd2b86d241f16dcf57116
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2014-08-27 13:09:03 -0700

    method for logifying data and uncertainties

---
+++ datautils.py
@@ -0,0 +1,27 @@
+import numpy as np
+import warnings
+
+def logify(data, sigma, mask):
+    """
+    Convert data to ln(data) and uncertainty to fractional uncertainty
+    for use in additive GP models.  This involves filtering for
+    negative data values and replacing them with something else.
+    """
+
+    tiny = 0.01 * data[data > 0].min()
+    bad = data < tiny
+    nbad = bad.sum()
+    
+    if nbad == 0:
+        return np.log(data), sigma/data, mask
+    
+    else:
+        warnings.warn("Setting {0} datapoints to \
+        {1} to insure positivity.".format(nbad, tiny))
+        data[bad] = tiny
+        sigma[bad] = np.sqrt(sigma[bad]**2 + (data[bad] - tiny)**2)
+        return np.log(data), sigma/data, mask
+    
+
+
+        
