352ccef86c808832cf47daaef4a49eae48a8b39a
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2016-02-14 15:26:09 -0500

    basic plotting utilities

---
+++ bsfh/utils/plotting.py
@@ -0,0 +1,178 @@
+import numpy as np
+import matplotlib.pyplot as pl
+
+__all__ = ["get_best", "get_truths", "get_percentiles", "get_stats",
+           "hist_samples", "joint_pdf", "compute_sigma_level", "get_prior",
+           "trim_walkers", "fill_between", "figgrid"]
+
+def get_best(res, **kwargs):
+    """Get the maximum a posteriori parameters.
+    """
+    imax = np.argmax(res['lnprobability'])
+    i, j = np.unravel_index(imax, res['lnprobability'].shape)
+    theta_best = res['chain'][i,j,:].copy()
+    theta_names = res.get('theta_labels', res['model'].theta_labels())
+    return theta_names, theta_best
+
+
+def get_truths(res):
+    import pickle
+    try:
+        mock = pickle.loads(res['obs']['mock_params'])
+        res['obs']['mock_params'] = mock
+    except:
+        pass
+    try:
+        return res['obs']['mock_params']
+    except(KeyError):
+        return None
+
+
+def get_prior(res, pname):
+    mp = res['model_params']
+    mpn = [m['name'] for m in mp]
+    ind = mpn.index(pname)
+    return mp[ind]['prior_function'], mp[ind]['prior_args']
+
+
+def get_percentiles(res, ptile=[16, 50, 84], start=0.75, thin=10, **extras):
+    """Get get percentiles of the marginalized posterior for each parameter.
+    """
+    nw, niter = res['chain'].shape[:-1]
+    parnames = np.array(res.get('theta_labels', res['model'].theta_labels()))
+    start_index = np.floor(start * (niter-1)).astype(int)
+    flatchain = res['chain'][:,start_index::thin,:]
+    dims = flatchain.shape
+    flatchain = flatchain.reshape(dims[0]*dims[1], dims[2])
+    pct = np.percentile(flatchain, ptile, axis=0)
+    return dict(zip(parnames, pct.T))
+
+    
+def get_stats(res, pnames, **kwargs):
+    truth = get_truths(res)
+    best = dict(zip(*get_best(res)))
+    pct = get_percentiles(res, **kwargs)
+    
+    truths = np.array([truth[k] for k in pnames])
+    pcts = np.array([pct[k] for i,k in enumerate(pnames)])
+    bests = np.array([best[k] for i,k in enumerate(pnames)])
+
+    return pnames, truths, bests, pcts 
+
+
+def trim_walkers(res, threshold=-1e4):
+    """Remove walkers with probability below some threshold.  Useful for
+    removing stuck walkers
+    """
+    good = res['lnprobability'][:,-1] > threshold
+    trimmed = {}
+    trimmed['chain'] = res['chain'][good,:,:]
+    trimmed['lnprobability'] = res['lnprobability'][good,:]
+    trimmed['model'] = res['model']
+    return trimmed
+
+
+def joint_pdf(res, p1, p2, pmap={}, **kwargs):
+    """Build a 2-dimensional array representing the binned joint PDF of 2
+    parameters, in terms of sigma or fraction of the total distribution.
+
+    For example, to plot contours of the joint PDF of parameters ``"parname1"``
+    and ``"parname2"`` from the last half of a chain with 30bins in each
+    dimension;
+
+    ::
+
+        xb, yb, sigma = joint_pdf(res, parname1, parname2, nbins=30, start=0.5)
+        ax.contour(xb, yb, sigma, **plotting_kwargs)
+
+    :param p1:
+        The name of the parameter for the x-axis
+
+    :param p2:
+        The name of the parameter for the y axis
+
+    :returns xb, yb, sigma:
+        The bins and the 2-d histogram
+    """
+    trace, pars = hist_samples(res, [p1, p2], **kwargs)
+    trace = trace.copy().T
+    if pars[0] == p1:
+        trace = trace[::-1, :]
+    x = pmap.get(p2, lambda x: x)(trace[0])
+    y = pmap.get(p1, lambda x: x)(trace[1])
+    xbins, ybins, sigma = compute_sigma_level(x, y, **kwargs)
+    return xbins, ybins, sigma.T
+
+
+def hist_samples(res, showpars=None, start=0, thin=1,
+                 return_lnprob=False, **extras):
+    """Get posterior samples for the parameters listed in ``showpars``.  This
+    can be done for different ending fractions of the (thinned) chain.
+    """
+    nw, niter = res['chain'].shape[:-1]
+    parnames = np.array(res.get('theta_labels', res['model'].theta_labels()))
+    start_index = np.floor(start * (niter-1)).astype(int)
+    flatchain = res['chain'][:,start_index::thin,:]
+    dims = flatchain.shape
+    flatchain = flatchain.reshape(dims[0]*dims[1], dims[2])
+    if showpars is None:
+        ind_show = np.ones(len(parnames), dtype=bool)
+    else:
+        ind_show = np.array([p in showpars for p in parnames], dtype= bool)
+    flatchain = flatchain[:, ind_show]
+    if return_lnprob:
+        flatlnprob = res['lnprobability'][:, start_index::thin].reshape(dims[0]*dims[1])
+        return flatchain, parnames[ind_show], flatlnprob
+    
+    return flatchain, parnames[ind_show]
+
+
+def compute_sigma_level(trace1, trace2, nbins=30, **extras):
+    """From a set of traces in two parameters, make a 2-d histogram of number
+    of standard deviations.  Following examples from J Vanderplas.
+    """
+    L, xbins, ybins = np.histogram2d(trace1, trace2, nbins,
+                                     range=extras.get('range', None))
+    L[L == 0] = 1E-16
+    logL = np.log(L)
+
+    shape = L.shape
+    L = L.ravel()
+
+    # obtain the indices to sort and unsort the flattened array
+    i_sort = np.argsort(L)[::-1]
+    i_unsort = np.argsort(i_sort)
+
+    L_cumsum = L[i_sort].cumsum()
+    L_cumsum /= L_cumsum[-1]
+    
+    xbins = 0.5 * (xbins[1:] + xbins[:-1])
+    ybins = 0.5 * (ybins[1:] + ybins[:-1])
+
+    return xbins, ybins, L_cumsum[i_unsort].reshape(shape)
+
+
+def figgrid(ny, nx):
+    from matplotlib import gridspec
+    fig = pl.figure(figsize=(nx*4.5, ny*3))
+    axarray = np.zeros([ny, nx], dtype=np.dtype('O'))
+    gs1 = gridspec.GridSpec(ny, nx)
+    gs1.update(left=0.1, right=0.85, top=0.9, bottom=0.1, wspace=0.2, hspace=0.10)
+    for i in range(ny):
+        for j in range(nx):
+            axarray[i,j] = fig.add_subplot(gs1[i,j])
+    return fig, axarray
+
+
+def fill_between(x, y1, y2=0, ax=None, **kwargs):
+    """Plot filled region between `y1` and `y2`.
+
+    This function works exactly the same as matplotlib's fill_between, except
+    that it also plots a proxy artist (specifically, a rectangle of 0 size)
+    so that it can be added it appears on a legend.
+    """
+    ax = ax if ax is not None else pl.gca()
+    ax.fill_between(x, y1, y2, **kwargs)
+    p = pl.Rectangle((0, 0), 0, 0, **kwargs)
+    ax.add_patch(p)
+    return p
