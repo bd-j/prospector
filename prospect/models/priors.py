46f8f1ec96b50d77e44fc28d39d4ea7fc7c0f247
John Moustakas <john.moustakas@gmail.com>
2017-06-28 10:28:49 -0400

    ignore log of zero

--- prospect/models/priors.py
+++ prospect/models/priors.py
@@ -2,6 +2,7 @@
 # These return the ln-prior-probability
 import numpy as np
 import scipy.stats
+import warnings
 
 __all__ = ["normal", "tophat", "normal_clipped", "lognormal", "logarithmic",
            "plotting_range",
@@ -138,7 +139,9 @@ class Prior(object):
             self.update(**kwargs)
         p = self.distribution.pdf(x, *self.args,
                                   loc=self.loc, scale=self.scale)
-        return np.log(p)
+        with np.errstate(invalid='ignore'):
+            lnp = np.log(p)
+        return lnp
 
     def sample(self, nsample=None, **kwargs):
         """Draw a sample from the prior distribution.
