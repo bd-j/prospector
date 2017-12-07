b3f8e51e57ffc37d5533180eceece0406ae9f2c9
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2017-12-07 18:06:20 -0500

    fix constants module, update docs and interactive demo.

--- prospect/sources/constants.py
+++ prospect/sources/constants.py
@@ -1,12 +1,13 @@
 import numpy as np
 
-__all__ == ['lsun', 'pc', 'lightspeed', 'ckms',
-            'jansky_mks', 'jansky_cgs',
-            'to_cgs_at_10pc', 'loge']
+__all__ = ['lsun', 'pc', 'lightspeed', 'ckms',
+           'jansky_mks', 'jansky_cgs',
+           'to_cgs_at_10pc', 'loge',
+           'kboltz', 'hplanck']
 
 
 # Useful constants
-lsun = 3.846e33
+lsun = 3.846e33  # erg/s
 pc = 3.085677581467192e18  # in cm
 lightspeed = 2.998e18  # AA/s
 ckms = 2.998e5  # km/s
@@ -14,6 +15,10 @@ jansky_mks = 1e-26
 jansky_cgs = 1e-23
 # value to go from L_sun/AA to erg/s/cm^2/AA at 10pc
 to_cgs_at_10pc = lsun / (4.0 * np.pi * (pc*10)**2)
+
+# cgs physical constants
+kboltz = 1.3806488e-16
+hplanck = 6.62606957e-27
 
 # change base
 loge = np.log10(np.e)
