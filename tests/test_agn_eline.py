ba6548cd8202d7432d9c60de4e237cb4e3e72d65
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2022-08-18 10:48:27 -0400

    format agn test for actually running in CI.

--- tests/test_agn_eline.py
+++ tests/test_agn_eline.py
@@ -12,12 +12,11 @@ from prospect.models.templates import TemplateLibrary
 from prospect.sources import CSPSpecBasis
 
 
-if __name__ == "__main__":
-
-    # sps
+def test_agn_elines():
+    # --- sps ---
     sps = CSPSpecBasis(zcontinuous=1)
 
-    # obs
+    # --- simple obs ---
     fnames = [f"sdss_{b}0" for b in "ugriz"]
     filts = observate.load_filters(fnames)
 
@@ -41,12 +40,16 @@ if __name__ == "__main__":
     spec0, phot0, x0 = model.predict(model.theta, obs, sps)
     model.params["agn_elum"] = 1e-6
     spec1, phot1, x1 = model.predict(model.theta, obs, sps)
+    assert (not np.allclose(spec2, spec0)), "changing AGN luminosity had no effect"
 
     model.params["agn_elum"] = 1e-4
     model.params["agn_eline_sigma"] = 400.0
     spec2, phot2, x2 = model.predict(model.theta, obs, sps)
-    assert np.allclose(phot2, phot0)
+    assert (not np.allclose(spec2, spec0)), "broadening lines had no effect on the spectrum"
 
+    assert np.allclose(phot2, phot0), "broadening lines changed the photometry"
+
+    # do a check for phot-only obs
     pobs = dict(filters=filts,
                 maggies=np.ones(len(filts)),
                 maggies_unc=0.1 * np.ones(len(filts)),
@@ -54,16 +57,17 @@ if __name__ == "__main__":
                 spectrum=None)
     pobs = fix_obs(pobs)
     spec3, phot3, x2 = model.predict(model.theta, obs=pobs, sps=sps)
-    assert np.allclose(phot3, phot2)
+    assert np.allclose(phot3, phot2), "Phot-only obs did not add AGn lines correctly"
 
-    pl.ion()
-    fig, ax = pl.subplots()
-    ax.plot(obs["wavelength"], spec0, label="agn_elum=1e-4")
-    ax.plot(obs["wavelength"], spec1, label="agn_elum=1e-6")
-    ax.plot(obs["wavelength"], spec2, label="agn_elum=1e-4, agn_sigma=400km/s")
-    ax.legend()
-    ax.set_xlabel("Wavelength")
-    ax.set_ylabel("F_nu (maggies)")
+    if False:
+        pl.ion()
+        fig, ax = pl.subplots()
+        ax.plot(obs["wavelength"], spec0, label="agn_elum=1e-4")
+        ax.plot(obs["wavelength"], spec1, label="agn_elum=1e-6")
+        ax.plot(obs["wavelength"], spec2, label="agn_elum=1e-4, agn_sigma=400km/s")
+        ax.legend()
+        ax.set_xlabel("Wavelength")
+        ax.set_ylabel("F_nu (maggies)")
 
-    print("Change in magnitude for faint AGN:\n", -2.5*np.log10(phot1/phot0))
-    print("Change in magnitude for broader-line AGN:\n", -2.5*np.log10(phot2/phot0))
+        print("Change in magnitude for faint AGN:\n", -2.5*np.log10(phot1/phot0))
+        print("Change in magnitude for broader-line AGN:\n", -2.5*np.log10(phot2/phot0))
