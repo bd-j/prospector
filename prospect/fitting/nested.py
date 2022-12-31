bd72fc4a0983eb692ae305170e6256f405d963f2
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2022-12-31 18:37:34 -0500

    fixes for dynesty >= v2.0, h/t @mjastro.

--- prospect/fitting/nested.py
+++ prospect/fitting/nested.py
@@ -33,28 +33,50 @@ def run_nestle_sampler(lnprobfn, model, verbose=True,
     return result
 
 
-def run_dynesty_sampler(lnprobfn, prior_transform, ndim, verbose=True,
-                        nested_bound='multi', nested_sample='unif',
-                        nested_nlive_init=100, nested_nlive_batch=100,
-                        nested_update_interval=0.6, nested_walks=25,
-                        nested_maxcall=None, nested_maxiter_init=None,
-                        pool=None, queue_size=1, nested_use_stop=True,
-                        nested_maxbatch=None, nested_weight_kwargs={'pfrac': 1.0},
-                        nested_bootstrap=0, nested_dlogz_init=0.02,
-                        use_pool={}, nested_first_update={},
-                        nested_maxcall_init=None, nested_live_points=None,
-                        nested_maxcall_batch=None, nested_maxiter=None,
-                        stop_function=None, wt_function=None,
-                        nested_maxiter_batch=None, nested_stop_kwargs={},
-                        nested_save_bounds=False,**kwargs):
+def run_dynesty_sampler(lnprobfn, prior_transform, ndim,
+                        verbose=True,
+                        # sampler kwargs
+                        nested_bound='multi',
+                        nested_sample='unif',
+                        nested_walks=25,
+                        nested_update_interval=0.6,
+                        nested_bootstrap=0,
+                        pool=None,
+                        use_pool={},
+                        queue_size=1,
+                        # init sampling kwargs
+                        nested_nlive_init=100,
+                        nested_dlogz_init=0.02,
+                        nested_maxiter_init=None,
+                        nested_maxcall_init=None,
+                        nested_live_points=None,
+                        # batch sampling kwargs
+                        nested_maxbatch=None,
+                        nested_nlive_batch=100,
+                        nested_maxiter_batch=None,
+                        nested_maxcall_batch=None,
+                        nested_use_stop=True,
+                        # overall kwargs
+                        nested_maxcall=None,
+                        nested_maxiter=None,
+                        nested_first_update={},
+                        stop_function=None,
+                        wt_function=None,
+                        nested_weight_kwargs={'pfrac': 1.0},
+                        nested_stop_kwargs={},
+                        nested_save_bounds=False,
+                        print_progress=True,
+                        **extras):
 
     # instantiate sampler
     dsampler = dynesty.DynamicNestedSampler(lnprobfn, prior_transform, ndim,
-                                            bound=nested_bound, sample=nested_sample,
+                                            bound=nested_bound,
+                                            sample=nested_sample,
+                                            walks=nested_walks,
+                                            bootstrap=nested_bootstrap,
                                             update_interval=nested_update_interval,
-                                            pool=pool, queue_size=queue_size,
-                                            walks=nested_walks, bootstrap=nested_bootstrap,
-                                            use_pool=use_pool)
+                                            pool=pool, queue_size=queue_size, use_pool=use_pool
+                                            )
 
     # generator for initial nested sampling
     ncall = dsampler.ncall
@@ -65,23 +87,33 @@ def run_dynesty_sampler(lnprobfn, prior_transform, ndim, verbose=True,
                                            maxcall=nested_maxcall_init,
                                            maxiter=nested_maxiter_init,
                                            live_points=nested_live_points):
-        (worst, ustar, vstar, loglstar, logvol,
-         logwt, logz, logzvar, h, nc, worst_it,
-         propidx, propiter, eff, delta_logz) = results
+
+        try:
+            # dynesty >= 2.0
+            (worst, ustar, vstar, loglstar, logvol,
+             logwt, logz, logzvar, h, nc, worst_it,
+             propidx, propiter, eff, delta_logz, blob) = results
+        except(ValueError):
+            # dynsety < 2.0
+            (worst, ustar, vstar, loglstar, logvol,
+            logwt, logz, logzvar, h, nc, worst_it,
+            propidx, propiter, eff, delta_logz) = results
+
         if delta_logz > 1e6:
             delta_logz = np.inf
         ncall += nc
         niter += 1
 
-        with np.errstate(invalid='ignore'):
-            logzerr = np.sqrt(logzvar)
-        sys.stderr.write("\riter: {:d} | batch: {:d} | nc: {:d} | "
-                         "ncall: {:d} | eff(%): {:6.3f} | "
-                         "logz: {:6.3f} +/- {:6.3f} | "
-                         "dlogz: {:6.3f} > {:6.3f}    "
-                         .format(niter, 0, nc, ncall, eff, logz,
-                                 logzerr, delta_logz, nested_dlogz_init))
-        sys.stderr.flush()
+        if print_progress:
+            with np.errstate(invalid='ignore'):
+                logzerr = np.sqrt(logzvar)
+            sys.stderr.write("\riter: {:d} | batch: {:d} | nc: {:d} | "
+                            "ncall: {:d} | eff(%): {:6.3f} | "
+                            "logz: {:6.3f} +/- {:6.3f} | "
+                            "dlogz: {:6.3f} > {:6.3f}    "
+                            .format(niter, 0, nc, ncall, eff, logz,
+                                    logzerr, delta_logz, nested_dlogz_init))
+            sys.stderr.flush()
 
     ndur = time.time() - tstart
     if verbose:
@@ -114,7 +146,7 @@ def run_dynesty_sampler(lnprobfn, prior_transform, ndim, verbose=True,
             stop, stop_vals = stop_function(res, nested_stop_kwargs,
                                             rstate=dsampler.rstate, M=M,
                                             return_vals=True)
-            stop_post, stop_evid, stop_val = stop_vals
+            stop_val = stop_vals[2]
         else:
             stop = False
             stop_val = np.NaN
@@ -131,27 +163,35 @@ def run_dynesty_sampler(lnprobfn, prior_transform, ndim, verbose=True,
                                                  maxiter=miter,
                                                  maxcall=mcall,
                                                  save_bounds=nested_save_bounds):
-                (worst, ustar, vstar, loglstar, nc,
-                 worst_it, propidx, propiter, eff) = results
+
+                try:
+                    # dynesty >= 2.0
+                    (worst, ustar, vstar, loglstar, nc,
+                     worst_it, propidx, propiter, eff, blob) = results
+                except(ValueError):
+                    # dynesty < 2.0
+                    (worst, ustar, vstar, loglstar, nc,
+                    worst_it, propidx, propiter, eff) = results
                 ncall += nc
                 niter += 1
-                sys.stderr.write("\riter: {:d} | batch: {:d} | "
-                                 "nc: {:d} | ncall: {:d} | "
-                                 "eff(%): {:6.3f} | "
-                                 "loglstar: {:6.3f} < {:6.3f} "
-                                 "< {:6.3f} | "
-                                 "logz: {:6.3f} +/- {:6.3f} | "
-                                 "stop: {:6.3f}    "
-                                 .format(niter, n+1, nc, ncall,
-                                         eff, logl_bounds[0], loglstar,
-                                         logl_bounds[1], lnz, lnzerr,
-                                         stop_val))
-                sys.stderr.flush()
+                if print_progress:
+                    sys.stderr.write("\riter: {:d} | batch: {:d} | "
+                                    "nc: {:d} | ncall: {:d} | "
+                                    "eff(%): {:6.3f} | "
+                                    "loglstar: {:6.3f} < {:6.3f} "
+                                    "< {:6.3f} | "
+                                    "logz: {:6.3f} +/- {:6.3f} | "
+                                    "stop: {:6.3f}    "
+                                    .format(niter, n+1, nc, ncall,
+                                            eff, logl_bounds[0], loglstar,
+                                            logl_bounds[1], lnz, lnzerr,
+                                            stop_val))
+                    sys.stderr.flush()
             dsampler.combine_runs()
         else:
             # We're done!
             break
-    
+
     ndur = time.time() - tstart
     if verbose:
         print('done dynesty (dynamic) in {0}s'.format(ndur))
