7dbfdb73c3b64dd2912a47cb57b865bbf79ed846
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2024-09-03 19:58:51 -0400

    move spectroscopic response function modeling to separate module.

--- prospect/observation/__init__.py
+++ prospect/observation/__init__.py
@@ -3,11 +3,11 @@
 from .observation import Observation
 from .observation import Photometry, Spectrum, Lines
 from .observation import UndersampledSpectrum, IntrinsicSpectrum
-from .observation import PolyOptCal, SplineOptCal
+from .response import PolyOptCal, SplineOptCal
 from .observation import from_oldstyle, from_serial
 
 __all__ = ["Observation",
            "Photometry", "Spectrum", "Lines",
-           "UndersampledSpectrum", "InstrinsicSpectrum",
+           "UndersampledSpectrum", "IntrinsicSpectrum",
            "PolyOptCal", "SplineOptCal",
            "from_oldstyle", "from_serial"]
